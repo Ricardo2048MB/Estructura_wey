8ddc1cd3ed221d1f5362b4b69805c0c8850be665
Ricardo Muñoz Bolaños <ricardo.munozb@my.unitec.edu.mx>
2023-05-18 13:36:25 -0600

    Commit inicial

---
+++ C/suma.c
@@ -0,0 +1,10 @@
+#include stdio.h
+main()
+{
+	int x = 0;
+	int y = 0;
+	
+	printf("Dame un número entero")
+	x = scanf("")
+	y = scanf("")
+}
---
+++ Python/sumas.py
@@ -0,0 +1,4 @@
+x = int(input("Dame un número natural positivo: "))
+y = int(input("Dame otro número natural positivo: "))
+suma = x + y
+print(f'Suma de {x} + {y} = {suma}')
